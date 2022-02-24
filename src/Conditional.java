229fa1f686a508396a0def0da99f5d732330ab54
hyeon21 <runngun21@naver.com>
2022-02-24 13:30:17 +0900

    if문 6. 오븐시계 7. 주사위 세개 추가

--- src/Conditional.java
+++ src/Conditional.java
@@ -8,7 +8,11 @@ public class Conditional {
 
     public static void main(String[] args) {
         Conditional cond = new Conditional();
-        cond.compare();
+        try {
+            cond.ovenClock();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -114,4 +118,69 @@ public class Conditional {
         br.close();
         System.out.println(modifiedHour + " " + modifiedMin);
     }
+
+    public void ovenClock() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int startHour = Integer.parseInt(st.nextToken());
+        int startMin = Integer.parseInt(st.nextToken());
+        int cookTime = Integer.parseInt(br.readLine());
+
+        startMin += cookTime;
+
+        if (59 < startMin) {
+            int plusHour = startMin / 60;
+            startHour += plusHour;
+            if (23 < startHour) {
+                startHour -= 24;
+            }
+            startMin -= (60 * plusHour);
+        }
+
+        br.close();
+
+        System.out.println(startHour + " " + startMin);
+
+    }
+
+    public void diceGame() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int firstDice = Integer.parseInt(st.nextToken());
+        int secondDice = Integer.parseInt(st.nextToken());
+        int thirdDice = Integer.parseInt(st.nextToken());
+
+        int prize = 0;
+
+        if (firstDice == secondDice) {
+            if (firstDice == thirdDice) {
+                prize = 10000 + (1000 * firstDice);
+            } else {
+                prize = 1000 + (100 * firstDice);
+            }
+        } else if (firstDice == thirdDice) {
+            prize = 1000 + (100 * firstDice);
+        } else if (secondDice == thirdDice) {
+            prize = 1000 + (100 * secondDice);
+        } else {
+            prize = 100 * getMaxNum(firstDice, secondDice, thirdDice);
+        }
+
+        System.out.println(prize);
+    }
+
+    public int getMaxNum(int a, int b, int c) {
+        int maxNum = a;
+
+        if (maxNum < b) {
+            maxNum = b;
+        }
+        if (maxNum < c) {
+            maxNum = c;
+        }
+
+        return maxNum;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,83 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        inThailand();
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.diceGame();
+
     }
 
-    public static void inThailand() {
-        int minusYear = 543;
-        Scanner scanner = new Scanner(System.in);
-        int year = Integer.parseInt(scanner.next());
+    public void ovenClock() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(year - minusYear);
+        int startHour = Integer.parseInt(st.nextToken());
+        int startMin = Integer.parseInt(st.nextToken());
+        int cookTime = Integer.parseInt(br.readLine());
+
+        startMin += cookTime;
+
+        if (59 < startMin) {
+            int plusHour = startMin / 60;
+            startHour += plusHour;
+            if (23 < startHour) {
+                startHour -= 24;
+            }
+            startMin -= (60 * plusHour);
+        }
+
+        br.close();
+
+        System.out.println(startHour + " " + startMin);
+
     }
+
+
+
+    public void diceGame() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int firstDice = Integer.parseInt(st.nextToken());
+        int secondDice = Integer.parseInt(st.nextToken());
+        int thirdDice = Integer.parseInt(st.nextToken());
+
+        int prize = 0;
+
+        if (firstDice == secondDice) {
+            if (firstDice == thirdDice) {
+                prize = 10000 + (1000 * firstDice);
+            } else {
+                prize = 1000 + (100 * firstDice);
+            }
+        } else if (firstDice == thirdDice) {
+            prize = 1000 + (100 * firstDice);
+        } else if (secondDice == thirdDice) {
+            prize = 1000 + (100 * secondDice);
+        } else {
+            prize = 100 * getMaxNum(firstDice, secondDice, thirdDice);
+        }
+
+        br.close();
+        System.out.println(prize);
+    }
+
+    public int getMaxNum(int a, int b, int c) {
+        int maxNum = a;
+
+        if (maxNum < b) {
+            maxNum = b;
+        }
+        if (maxNum < c) {
+            maxNum = c;
+        }
+
+        return maxNum;
+    }
+
 }
